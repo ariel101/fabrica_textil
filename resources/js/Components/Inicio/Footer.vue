9d5059964105c353e090b9aa4b5c2b04c97a88ca
ariel101 <ariel.cayo.vargas@gmail.com>
2026-06-17 14:00:47 -0400

    --

--- resources/js/Components/Inicio/Footer.vue
+++ resources/js/Components/Inicio/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="bg-gray-800 text-white text-center p-6 mt-10">
-      <p>&copy; 2025 Fábrica Textil. Todos los derechos reservados.</p>
+      <p>&copy; 2026 Fábrica Textil. Todos los derechos reservados.</p>
     </footer>
   </template>
   
